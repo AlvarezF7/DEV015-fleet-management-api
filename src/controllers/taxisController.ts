6b03e84aa6d39ab04b35edcfb2d267c72da19fe1
Falvarez <alvarez.vic.382@gmail.com>
2024-10-15 14:24:54 -0300

    modulariza taxicontroller y taxiModels

--- src/controllers/taxisController.ts
+++ src/controllers/taxisController.ts
@@ -1,6 +1,7 @@
 //codigo de la consulta para los taxis
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import { GetTaxis } from '../models/taxiModel';
 
 const prisma = new PrismaClient();
 
@@ -10,31 +11,14 @@ export const getAllTaxis = async (req: Request, res: Response): Promise<void> =>
         const limit = parseInt(req.query.limit as string) || 10;
         const plate = req.query.plate as string; // Obtiene plate
 
-        //manejo de error
-        if (isNaN(page) || page < 1) {
-            res.status(400).json({ error: 'pagina o limite invalido' });
-            return; 
-        }
-
-        if (isNaN(limit) || limit < 1) {
+        //manejo de error        
+        if (isNaN(page) || page < 1 || isNaN(limit) || limit < 1) {
             res.status(400).json({ error: 'pagina o limite invalido' });
             return;
         }
-
-        const taxis = await prisma.taxis.findMany({
-            skip: (page - 1) * limit,
-            take: limit,
-            where: plate ? {
-                plate: {
-                    startsWith: plate,   
-                    mode: 'insensitive',
-                },
-            } : undefined,
-            select: {
-                id: true,
-                plate: true,
-            },
-        });
+       
+       const taxis = await GetTaxis (page, limit, plate);
+          
         // Manejo de error
         if (taxis.length === 0) {
             res.status(400).json({ error: 'Página no encontrada' });
--- src/models/taxiModel.ts
+++ src/models/taxiModel.ts
@@ -1,5 +1,23 @@
 //aqui va el modelo Contienen la lógica para acceder a los datos. parece q los select tmb van aqui
-export interface Taxi {
-    id: number;
-    plate: string;
-}
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const GetTaxis = async (page: number,limit: number, plate?: string) => {
+    
+     return await prisma.taxis.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: plate ? {
+            plate: {
+                startsWith: plate,   
+                mode: 'insensitive',
+            },
+        } : undefined,
+        select: {
+            id: true,
+            plate: true,
+        },
+    });
+};
